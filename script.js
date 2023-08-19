854d2ede0e9ca13bbef2c57bd7b1addfda53ee5b
matorolll <mgajos00@gmail.com>
2023-08-19 17:25:36 +0200

    adding grid and onhover

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("init")
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+});
+
+//Mouse events
+let isMousePressed = false;
+document.addEventListener('mouseup', () => {
+    isMousePressed = false;
+});
+document.addEventListener('mousedown', () => {
+    isMousePressed = true;
+});
+
+
+//Grid events
+const gridContainer = document.getElementById("gridContainer")
+for(let i=0; i < 16 * 16; i++){
+    const div = document.createElement('div');
+    div.className = 'grid-item';
+
+
+    div.addEventListener('mouseenter', ()=>{
+        if(isMousePressed){
+            div.style.backgroundColor = 'red';
+        }
+    })
+
+    div.addEventListener('contentmenu', (event)=>{
+        if(event.button===2){
+            div.style.backgroundColor = 'lightgray';
+        }
+    })
+
+    div.addEventListener('mouseleave', ()=>{
+        if(!isMousePressed){
+           div.style.backgroundColor = 'lightgray';
+       }
+    })  
+
+
+    gridContainer.appendChild(div);
+}
